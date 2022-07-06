38eecbf2ec0c3d3414660a08a17a8b3d6fd5fd74
姚圣 <1076009113@qq.com>
2022-07-06 20:43:02 +0800

    Add ceil coding

--- src/main/java/org/yaosheng/algorithm/Binary_Search/BinarySearch.java
+++ src/main/java/org/yaosheng/algorithm/Binary_Search/BinarySearch.java
@@ -42,10 +42,20 @@ public class BinarySearch {
         return l;
     }
 
+    // > target，返回最小索引
+    // ==target，返回最大索引
+    public static <E extends Comparable<E>> int ceil(E[] data,E target){
+
+        int u = upper (data,target);
+        if(u - 1 >= 0 && data[u - 1].compareTo (target) == 0 )
+            return u - 1;
+        return u;
+    }
+
     public static void main(String[] args) {
 
         Integer[] arr = {1,1,3,3,5,5};
         for(int i = 0;i <= 6;i ++)
-            System.out.println (BinarySearch.upper (arr,i));
+            System.out.println (BinarySearch.ceil (arr,i));
     }
 }
