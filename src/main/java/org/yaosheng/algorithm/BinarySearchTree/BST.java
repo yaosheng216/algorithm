985fae7b58b140ff015798aef73ab12f60a116ef
姚圣 <1076009113@qq.com>
2022-07-10 22:55:51 +0800

    Add BST remove elements

--- src/main/java/org/yaosheng/algorithm/BinarySearchTree/BST.java
+++ src/main/java/org/yaosheng/algorithm/BinarySearchTree/BST.java
@@ -154,6 +154,129 @@ public class BST <E extends Comparable<E>> {
         }
     }
 
+    // 查找二分搜索树的最小元素
+    public E minimum(){
+
+        if(size == 0)
+            throw new IllegalArgumentException ("BST is empty");
+
+        return  minimun (root).e;
+    }
+
+    // 返回以node为根的二分搜索树的最小值所在节点
+    private Node minimun(Node node){
+
+        if(node.left == null)
+            return node;
+        return minimun (node.left);
+    }
+
+    // 寻找二分搜索树的最大元素
+    public E maximum(){
+
+        if(size == 0)
+            throw new IllegalArgumentException ("BST is empty");
+
+        return maximum (root).e;
+    }
+
+    private Node maximum(Node node){
+
+        if(node.right == null)
+            return node;
+
+        return maximum (node.right);
+    }
+
+    // 从二分搜索树中删除最小值所在节点，返回最小值
+    public E removeMin(){
+
+        E ret = minimum ();
+        root = removeMin (root);
+        return ret;
+    }
+
+    // 向以node为根的二分搜索树中插入元素e，递归算法
+    // 返回插入新节点后的二分搜索树的根
+    private Node removeMin(Node node){
+
+        if(node.left == null){
+            Node rightNode = node.right;
+            node.right = null;
+            size --;
+            return rightNode;
+        }
+
+        node.left = removeMin (node.left);
+        return node;
+    }
+
+    // 从二分搜索树中删除最大值所在的节点，返回最大值
+    public E removeMax(){
+
+        E ret = maximum ();
+        root = removeMax (root);
+        return ret;
+    }
+
+    private Node removeMax(Node node){
+
+        if(node.right == null){
+            Node leftNode = node.left;
+            node.left = null;
+            size --;
+            return leftNode;
+        }
+
+        node.right = removeMax (node.right);
+        return node;
+    }
+
+    // 从二分搜索树中删除元素为e的节点
+    public void remove(E e){
+        root = remove(root,e);
+    }
+
+    private Node remove(Node node,E e){
+
+        if(node == null)
+            return null;
+
+        if(e.compareTo(node.e) < 0){
+            node.left = remove(node.left , e);
+            return node;
+        }else if(e.compareTo(node.e) > 0){
+            node.right = remove(node.right, e);
+            return node;
+        }else{
+            // 待删除节点左子树为空的情况
+            if(node.left == null){
+                Node rightNode = node.right;
+                node.right = null;
+                size --;
+                return rightNode;
+            }
+
+            // 待删除节点右子树为空的情况
+            if(node.right == null){
+                Node leftNode = node.left;
+                node.left = null;
+                size --;
+                return leftNode;
+            }
+            // 待删除节点左右子树均不为空的情况
+            // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+            // 用这个节点顶替待删除节点的位置
+            Node successor = minimun (node.right);
+            successor.right = removeMin(node.right);
+            successor.left = node.left;
+
+            node.left = node.right = null;
+
+            return successor;
+        }
+    }
+
     @Override
     public String toString(){
         StringBuilder res = new StringBuilder ();
--- src/main/java/org/yaosheng/algorithm/BinarySearchTree/BSTMain.java
+++ src/main/java/org/yaosheng/algorithm/BinarySearchTree/BSTMain.java
@@ -1,5 +1,8 @@
 package org.yaosheng.algorithm.BinarySearchTree;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by yaosheng on 2022/7/7.
  */
@@ -8,6 +11,7 @@ public class BSTMain {
     public static void main(String[] args) {
 
         BST<Integer> bst = new BST<> ();
+        Random random = new Random ();
         int[] nums = {5,3,6,8,4,2};
         for(int num : nums)
             bst.add (num);
@@ -22,5 +26,39 @@ public class BSTMain {
 
         bst.postOrder ();
         System.out.println ();
+
+        int n = 1000;
+
+        for(int i = 0;i < n;i ++){
+            bst.add (random.nextInt (10000));
+        }
+
+        ArrayList<Integer> list = new ArrayList<> ();
+        while(!bst.isEmpty ()){
+            list.add (bst.removeMin ());
+        }
+        System.out.println (list);
+
+        for(int i = 1;i < list.size ();i ++)
+            if(list.get (i - 1) > list.get (i))
+                throw new IllegalArgumentException ("Error");
+
+        System.out.println ("removeMin test completed");
+
+        for(int i = 0;i < n;i ++){
+            bst.add (random.nextInt (10000));
+        }
+
+        list = new ArrayList<> ();
+        while(!bst.isEmpty ()){
+            list.add (bst.removeMax ());
+        }
+        System.out.println (list);
+
+        for(int i = 1;i < list.size ();i ++)
+            if(list.get (i - 1) > list.get (i))
+                throw new IllegalArgumentException ("Error");
+
+        System.out.println ("removeMax test completed");
     }
 }
