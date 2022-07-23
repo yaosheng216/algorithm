aee1e24c7d4c1818c7efa4314de9fa0d3ff6a02d
姚圣 <1076009113@qq.com>
2022-07-23 15:51:33 +0800

    Add BubbleSort coding

--- src/main/java/org/yaosheng/algorithm/BubbleSort/ShellSort.java
+++ src/main/java/org/yaosheng/algorithm/BubbleSort/ShellSort.java
@@ -17,16 +17,15 @@ public class ShellSort {
 
         int h = data.length / 2;
         while(h >= 1){
-            for(int start = 0;start < h;start ++){
-                // 对data[start,start + h,start + 2h,start + 3h ...]，进行插入排序
-                for(int i = start + h;i < data.length;i += h){
-                    E t = data[i];
-                    int j;
-                    for(j = i;j - h >= 0 && t.compareTo (data[j - h]) < 0;j -= h){
-                        data[j] = data[j - h];
-                    }
-                    data[j] = t;
+
+            // 对data[h,n)进行插入排序
+            for(int i = h;i < data.length;i ++){
+                E t = data[i];
+                int j;
+                for(j = i;j - h >= 0 && t.compareTo (data[j - h]) < 0;j -= h){
+                    data[j] = data[j - h];
                 }
+                data[j] = t;
             }
             h /= 2;
         }
