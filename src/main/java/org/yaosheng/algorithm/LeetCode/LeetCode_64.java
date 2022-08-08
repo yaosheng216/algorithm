64db53469d0939f82b315ec41e814b3e49939213
姚圣 <1076009113@qq.com>
2022-08-08 22:07:42 +0800

    Push leetcode coding

--- src/main/java/org/yaosheng/algorithm/LeetCode/LeetCode_64.java
+++ src/main/java/org/yaosheng/algorithm/LeetCode/LeetCode_64.java
@@ -7,28 +7,22 @@ public class LeetCode_64 {
 
     public static int minPathSum(int[][] grid){
 
-        if(grid.length == 0)
+        int width = grid[0].length, high = grid.length;
+        if (high == 0 || width == 0)
             return 0;
 
-        int x = 0,y = 0;
-        int res = grid[x][y];
+        // 初始化
+        for (int i = 1; i < high; i ++)
+            grid[i][0] += grid[i - 1][0];
 
-        while(x <= grid.length && y <= grid[grid.length - 1].length){
-           int right = res + grid[x][y + 1];
-           int next = res + grid[x + 1][y];
-           if(y == grid[1].length - 1){
-               x ++;
-               res += grid[x][y];
-               continue;
-           }
-           if(right < next){
-               y ++;
-           }else{
-               x ++;
-           }
-           res = Math.min (right,next);
-        }
-        return res;
+        for (int i = 1; i < width; i ++)
+            grid[0][i] += grid[0][i - 1];
+
+        for (int i = 1; i < high; i ++)
+            for (int j = 1; j < width; j++)
+                grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+
+        return grid[high - 1][width - 1];
     }
 
     public static void main(String[] args) {
