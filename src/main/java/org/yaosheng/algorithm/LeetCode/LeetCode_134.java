bccdefca6c5c2651dfbe3a5adba413c5ca8fc2a8
姚圣 <1076009113@qq.com>
2022-09-21 22:00:21 +0800

    Push update

--- src/main/java/org/yaosheng/algorithm/LeetCode/LeetCode_134.java
+++ src/main/java/org/yaosheng/algorithm/LeetCode/LeetCode_134.java
@@ -10,6 +10,7 @@ public class LeetCode_134 {
         int curSum = 0;
         int totalSum = 0;
         int index = 0;
+
         for (int i = 0; i < gas.length; i ++) {
             curSum += gas[i] - cost[i];
             totalSum += gas[i] - cost[i];
